addc0a2f3f5283a59e36961b6fb0e63f07d7ecc7
Oscar <oscarcalvert01@gmail.com>
2023-12-12 14:46:06 -0500

    added light mode for desktop

---
+++ colors.js
@@ -0,0 +1,24 @@
+// javascript code heavily inspired by https://stackoverflow.com/questions/14292997/changing-style-sheet-javascript
+document.getElementById('switch-style').addEventListener('click', function () {
+
+    var colorpreference = document.getElementById('stylesheet');
+    if (colorpreference.getAttribute('href') == './styles/main.css') {
+        colorpreference.setAttribute('href', './styles/mainLight.css');
+        localStorage.setItem('stylesheet', './styles/mainLight.css');
+    }
+    else {
+        colorpreference.setAttribute('href', './styles/main.css');
+        localStorage.setItem('stylesheet', './styles/main.css');
+        
+    }
+});
+
+window.onload = function() {
+
+    var colorpreference = document.getElementById('stylesheet');
+    var siteColor = localStorage.getItem('stylesheet');
+
+    if (siteColor) {
+        colorpreference.setAttribute('href', stylesheet);
+    }
+ };
