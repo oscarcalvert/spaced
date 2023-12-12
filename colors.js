2963f05d8c226796499c610c077582a1d78b6bc6
Oscar <oscarcalvert01@gmail.com>
2023-12-12 14:57:39 -0500

    fixed minor issues and colors

--- colors.js
+++ colors.js
@@ -1,7 +1,10 @@
 // javascript code heavily inspired by https://stackoverflow.com/questions/14292997/changing-style-sheet-javascript
+var colorpreference = document.getElementById('stylesheet');
+
 document.getElementById('switch-style').addEventListener('click', function () {
 
-    var colorpreference = document.getElementById('stylesheet');
+    
+
     if (colorpreference.getAttribute('href') == './styles/main.css') {
         colorpreference.setAttribute('href', './styles/mainLight.css');
         localStorage.setItem('stylesheet', './styles/mainLight.css');
@@ -14,8 +17,7 @@ document.getElementById('switch-style').addEventListener('click', function () {
 });
 
 window.onload = function() {
-
-    var colorpreference = document.getElementById('stylesheet');
+    
     var siteColor = localStorage.getItem('stylesheet');
 
     if (siteColor) {
