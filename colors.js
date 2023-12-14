efc42040029e7318ffdd96ead7829592045a84c2
Oscar <oscarcalvert01@gmail.com>
2023-12-13 20:03:21 -0500

    added header interactivity... again

--- colors.js
+++ colors.js
@@ -1,37 +1,30 @@
-// javascript code heavily inspired by https://stackoverflow.com/questions/14292997/changing-style-sheet-javascript
+
 var colorpreference = document.getElementById('stylesheet');
-
-
-document.getElementById('switch-style').addEventListener('click', function () {
-
-    
-
-    if (colorpreference.getAttribute('href') == './styles/main.css') {
-        colorpreference.setAttribute('href', './styles/mainLight.css');
-        localStorage.setItem('stylesheet', './styles/mainLight.css');
-    }
-    else {
-        colorpreference.setAttribute('href', './styles/main.css');
-        localStorage.setItem('stylesheet', './styles/main.css');
-        
-    }
-});
-
-window.onload = function() {
-    
+var headercolor = document.getElementById('desktop-header-placeholder')
+window.onload = function () {
     var siteColor = localStorage.getItem('stylesheet');
-
     if (siteColor) {
         colorpreference.setAttribute('href', siteColor);
     }
- };
+};
+document.getElementById('switch-style').addEventListener('click', function () { //code heavily inspired by https://stackoverflow.com/questions/14292997/changing-style-sheet-javascript
+    if (colorpreference.getAttribute('href') == './styles/main.css') {
+        colorpreference.setAttribute('href', './styles/mainLight.css');
+        localStorage.setItem('stylesheet', './styles/mainLight.css');
+    } else {
+        colorpreference.setAttribute('href', './styles/main.css');
+        localStorage.setItem('stylesheet', './styles/main.css');
+
+    }
+});
 
 document.addEventListener("scroll", function () {
-    var headercolor = document.getElementById('desktop-header-placeholder')
     if (window.scrollY > 100) {
         headercolor.setAttribute('id', 'desktop-header')
     }
-    else {
+});
+document.addEventListener("scroll", function () {
+    if (window.scrollY <= 100) {
         headercolor.setAttribute('id', 'desktop-header-placeholder')
     }
- });
+})
